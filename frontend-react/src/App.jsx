b4fe3dc3458e25d82bb4edc7c15871014279d04c
Christopher Amah <christopheramah0@gmail.com>
2025-07-03 11:51:09 +0100

    homepage react

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
-
+import './assets/css/style.css'
+import Header from './assets/components/header'
+import Main from './assets/components/main'
+import Footer from './assets/components/footer'
 
 function App() {
 
   return (
     <>
-      
+      <Header />
+      <Main />
+      <Footer />
     </>
   )
 }
---
+++ frontend-react/src/assets/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <>
+        <footer className='bg-dark text-light text-center py-3 my-5'>
+            <hr className='border-bottom' />
+            <p className='text-light text-center'>&copy; 2025 - Built by Christopher Amah</p>
+        </footer>
+    </>
+  )
+}
+
+export default footer
