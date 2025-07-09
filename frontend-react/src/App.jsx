efe2c561bdf6a34ad2b89e181788b294581855e3
Christopher Amah <christopheramah0@gmail.com>
2025-07-09 11:01:26 +0100

    registration page

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,16 +1,25 @@
 import { useState } from 'react'
 import './assets/css/style.css'
-import Header from './assets/components/header'
 import Main from './assets/components/main'
+import Register from './assets/components/Register'
+import Login from './assets/components/Login'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './assets/components/header'
 import Footer from './assets/components/footer'
 
 function App() {
 
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+    <BrowserRouter>
+    <Header />
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        <Footer />
+    </BrowserRouter>
     </>
   )
 }
